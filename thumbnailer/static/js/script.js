298d11e60ba12b157e36540ce6b43bc4ea64b313
egorrybko <105800440+egorrybko@users.noreply.github.com>
2023-05-01 14:09:04 +0000

    skr edit

--- thumbnailer/static/js/script.js
+++ thumbnailer/static/js/script.js
@@ -3,12 +3,17 @@ let input = document.querySelector("#message-input")
 let btnSubmit = document.querySelector("#btn-submit")
  
 const webSocket = new WebSocket('ws://' + window.location.host + '/ws/chat/');
- 
+
+
 webSocket.onmessage = function(e) {
+    alert(`[message] Данные получены с сервера: ${e.data}`);
     const data = JSON.parse(e.data);
     chat.innerHTML += '<div class="msg">' + data.message + '</div>'
 };
- 
+
+webSocket.onerror = function(error) {
+    alert(`[error]`);
+};
  
 btnSubmit.addEventListener("click", () => {
     message = input.value;
